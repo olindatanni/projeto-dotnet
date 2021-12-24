3d9420cc46bfce9aea198f6d1f989311bb137344
olindatanni <tannimasao@gmail.com>
2021-12-24 10:27:46 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arrayInteiros = new int [3];
+            arrayInteiros [0] = 10;
+            arrayInteiros [1] = 20;
+            arrayInteiros [2] = 30;
+            System.Console.WriteLine ("Percorrendo o array pelo for");
+            for (int i = 0; i < arrayInteiros.Length; i++) 
+            {
+
+                     System.Console.WriteLine(arrayInteiros[i]);
+            }       
+        }
+    }
+}
